a1b722ad4b228e88194d8fd2a31379f801a452cc
rabuu <rabuu.dev@gmail.com>
2022-03-02 22:32:58 +0100

    Implement comment votes

--- server/src/request/comment/teacher_comment.rs
+++ server/src/request/comment/teacher_comment.rs
@@ -13,7 +13,11 @@ use babibapp_schema::schema;
 pub fn config(cfg: &mut web::ServiceConfig) {
     cfg.service(get)
         .service(get_all)
+        .service(get_vote)
         .service(create)
+        .service(do_upvote)
+        .service(do_downvote)
+        .service(do_unvote)
         .service(delete);
 }
 
@@ -92,6 +96,42 @@ async fn get_all(context: web::Data<RequestContext>, req: HttpRequest) -> Reques
     }
 }
 
+#[get("/get_vote/{comment_id}")]
+async fn get_vote(
+    context: web::Data<RequestContext>,
+    req: HttpRequest,
+    comment_id: web::Path<i32>,
+) -> RequestResult {
+    let token_settings = &context.settings.token;
+
+    let token = auth::TokenWrapper::from_request(req.clone())?;
+    let _ = token.validate(token_settings.secret.clone())?;
+
+    let query_comment_id = comment_id.into_inner();
+
+    let vote = db::blocked_access(&context.pool, move |conn| {
+        use diesel::dsl::count_star;
+        use schema::teacher_comment_votes::dsl::*;
+
+        let upvotes = teacher_comment_votes
+            .filter(comment_id.eq(query_comment_id))
+            .filter(upvote.eq(true))
+            .select(count_star())
+            .get_result::<i64>(conn)?;
+
+        let downvotes = teacher_comment_votes
+            .filter(comment_id.eq(query_comment_id))
+            .filter(upvote.eq(false))
+            .select(count_star())
+            .get_result::<i64>(conn)?;
+
+        Ok(upvotes - downvotes) as Result<i64, BabibappError>
+    })
+    .await??;
+
+    Ok(HttpResponse::Ok().json(vote))
+}
+
 #[post("/create")]
 async fn create(
     context: web::Data<RequestContext>,
@@ -122,6 +162,132 @@ async fn create(
     log::debug!("Database response: {:?}", comment);
 
     Ok(HttpResponse::Ok().json(comment))
+}
+
+#[post("/upvote/{comment_id}")]
+async fn do_upvote(
+    context: web::Data<RequestContext>,
+    req: HttpRequest,
+    comment_id: web::Path<i32>,
+) -> RequestResult {
+    let token_settings = &context.settings.token;
+
+    let token = auth::TokenWrapper::from_request(req.clone())?;
+    let claims = token.validate(token_settings.secret.clone())?;
+
+    let query_comment_id = comment_id.into_inner();
+
+    let comment_vote = db::blocked_access(&context.pool, move |conn| {
+        use schema::teacher_comment_votes::dsl::*;
+
+        diesel::update(teacher_comment_votes)
+            .filter(comment_id.eq(query_comment_id))
+            .filter(student_id.eq(claims.id))
+            .set(upvote.eq(true))
+            .get_result::<models::comment::TeacherCommentVote>(conn)
+            .optional()
+    })
+    .await??;
+
+    let comment_vote = if let Some(comment_vote) = comment_vote {
+        comment_vote
+    } else {
+        db::blocked_access(&context.pool, move |conn| {
+            use schema::teacher_comment_votes::dsl::*;
+
+            let new_comment_vote = models::comment::NewTeacherCommentVote {
+                comment_id: query_comment_id,
+                student_id: claims.id,
+                upvote: true,
+            };
+
+            diesel::insert_into(teacher_comment_votes)
+                .values(&new_comment_vote)
+                .get_result::<models::comment::TeacherCommentVote>(conn)
+        })
+        .await??
+    };
+
+    Ok(HttpResponse::Ok().json(comment_vote))
+}
+
+#[post("/downvote/{comment_id}")]
+async fn do_downvote(
+    context: web::Data<RequestContext>,
+    req: HttpRequest,
+    comment_id: web::Path<i32>,
+) -> RequestResult {
+    let token_settings = &context.settings.token;
+
+    let token = auth::TokenWrapper::from_request(req.clone())?;
+    let claims = token.validate(token_settings.secret.clone())?;
+
+    let query_comment_id = comment_id.into_inner();
+
+    let comment_vote = db::blocked_access(&context.pool, move |conn| {
+        use schema::teacher_comment_votes::dsl::*;
+
+        diesel::update(teacher_comment_votes)
+            .filter(comment_id.eq(query_comment_id))
+            .filter(student_id.eq(claims.id))
+            .set(upvote.eq(false))
+            .get_result::<models::comment::TeacherCommentVote>(conn)
+            .optional()
+    })
+    .await??;
+
+    let comment_vote = if let Some(comment_vote) = comment_vote {
+        comment_vote
+    } else {
+        db::blocked_access(&context.pool, move |conn| {
+            use schema::teacher_comment_votes::dsl::*;
+
+            let new_comment_vote = models::comment::NewTeacherCommentVote {
+                comment_id: query_comment_id,
+                student_id: claims.id,
+                upvote: false,
+            };
+
+            diesel::insert_into(teacher_comment_votes)
+                .values(&new_comment_vote)
+                .get_result::<models::comment::TeacherCommentVote>(conn)
+        })
+        .await??
+    };
+
+    Ok(HttpResponse::Ok().json(comment_vote))
+}
+
+#[delete("/unvote/{comment_id}")]
+async fn do_unvote(
+    context: web::Data<RequestContext>,
+    req: HttpRequest,
+    comment_id: web::Path<i32>,
+) -> RequestResult {
+    let token_settings = &context.settings.token;
+
+    let token = auth::TokenWrapper::from_request(req.clone())?;
+    let claims = token.validate(token_settings.secret.clone())?;
+
+    let query_comment_id = comment_id.into_inner();
+
+    let comment_vote = db::blocked_access(&context.pool, move |conn| {
+        use schema::teacher_comment_votes::dsl::*;
+        diesel::delete(
+            teacher_comment_votes
+                .filter(student_id.eq(claims.id))
+                .filter(comment_id.eq(query_comment_id)),
+        )
+        .get_result::<models::comment::TeacherCommentVote>(conn)
+        .optional()
+    })
+    .await??;
+
+    if let Some(comment_vote) = comment_vote {
+        Ok(HttpResponse::Ok().json(comment_vote))
+    } else {
+        Ok(HttpResponse::Ok().body(format!("No vote on comment: {}", query_comment_id)))
+    }
 }
 
 #[delete("/delete/{comment_id}")]
